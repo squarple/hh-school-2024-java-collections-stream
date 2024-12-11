974459f18de1d687917c88f59b2a742508440da4
Anton Radzionau <radionoff.antony@gmail.com>
2024-12-11 23:03:01 +0300

    task 8

--- src/main/java/tasks/Task8.java
+++ src/main/java/tasks/Task8.java
@@ -6,6 +6,7 @@ import common.PersonWithResumes;
 import common.Resume;
 import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
   Еще один вариант задачи обогащения
@@ -21,7 +22,12 @@ public class Task8 {
   }
 
   public Set<PersonWithResumes> enrichPersonsWithResumes(Collection<Person> persons) {
-    Set<Resume> resumes = personService.findResumes(Set.of());
-    return Set.of();
+    Set<Resume> resumes = personService.findResumes(persons.stream().map(Person::id).collect(Collectors.toList()));
+    
+    return persons.stream()
+        .map(person -> new PersonWithResumes(person, resumes.stream()
+            .filter(resume -> resume.personId().equals(person.id()))
+            .collect(Collectors.toSet())))
+        .collect(Collectors.toSet());
   }
 }
